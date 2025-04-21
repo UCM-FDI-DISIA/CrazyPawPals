2c4e8ccb7fddf5fb8280a80f1f3d8cc3ebd90174
bingoo30 <biwang01@ucm.es>
2025-04-22 01:56:00 +0200

    Update RewardScene.cpp

--- project/src/game/scenes/RewardScene.cpp
+++ project/src/game/scenes/RewardScene.cpp
@@ -662,7 +662,7 @@ void RewardScene::create_reward_exchange_button(const GameStructs::ButtonPropert
             _last_deck_card_img->destination_rect.position.y += 0.05f;
 
             auto imgNext = mngr->getComponent<ImageForButton>(mngr->getHandler(ecs::hdlr::NEXTROUNDREWARD));
-            imgNext->destination_rect.position = { 0.4f, 0.35f };
+            imgNext->destination_rect.position = { 0.35f, 0.32f };
             imgNext->destination_rect.size = { 0.3f, 0.125f * 1.25f };
 
             auto imgConfirm = mngr->getComponent<ImageForButton>(mngr->getHandler(ecs::hdlr::CONFIRMREWARD));
