93444336b1e8067fbca7eaf8a14d45d09d3a2df3
bingoo30 <biwang01@ucm.es>
2025-04-26 14:29:32 +0200

    CAMBIOS MAS SPRITES

--- project/src/game/scenes/SelectionMenuScene.cpp
+++ project/src/game/scenes/SelectionMenuScene.cpp
@@ -34,9 +34,9 @@ SelectionMenuScene::~SelectionMenuScene()
 }
 void SelectionMenuScene::create_weapon_buttons() {
     float umbral = 0.0075f;
-    float offsetX = 0.06f;  // Distance between buttons on the X axis
-    float startX = 0.7f;   // Starting position of the first button on X
-    float startY = 0.075f; // Starting position of the first button on Y
+    float offsetX = 0.07f;  // Distance between buttons on the X axis
+    float startX = 0.71f;   // Starting position of the first button on X
+    float startY = 0.2f; // Starting position of the first button on Y
 
     GameStructs::ButtonProperties buttonPropTemplate = {
         { { startX, startY }, {0.05f, 0.1f} },
@@ -62,10 +62,10 @@ void SelectionMenuScene::create_weapon_buttons() {
     rampCanonB.sprite_key = "ramp_canon_button";
     create_weapon_button(GameStructs::RAMP_CANON, rampCanonB);
 
-    buttonPropTemplate.rect.position.x += offsetX;  // Move to the right
-    GameStructs::ButtonProperties lightbringerB = buttonPropTemplate;
-    lightbringerB.sprite_key = "lightbringer_button";
-    create_weapon_button(GameStructs::LIGHTBRINGER, lightbringerB);
+    //buttonPropTemplate.rect.position.x += offsetX;  // Move to the right
+    //GameStructs::ButtonProperties lightbringerB = buttonPropTemplate;
+    //lightbringerB.sprite_key = "lightbringer_button";
+    //create_weapon_button(GameStructs::LIGHTBRINGER, lightbringerB);
 
     //we ll add these two when we have other weapon implemented
     //buttonPropTemplate.rect.position.x += offsetX;  // Move to the right
@@ -83,10 +83,10 @@ void SelectionMenuScene::create_weapon_buttons() {
 }
 
 void SelectionMenuScene::create_deck_buttons() {
-    float umbral = 0.0875f;
+    float umbral = 0.1f;
     //create the first button prop
     GameStructs::ButtonProperties buttonPropTemplate = {
-         { {0.0075f, 0.075f},{0.085f, 0.135f} },
+         { {0.0075f, 0.2f},{0.085f, 0.135f} },
          0.0f, "", ecs::grp::DECKBUTTON
     };
     GameStructs::ButtonProperties deck1B = buttonPropTemplate;
@@ -355,7 +355,7 @@ void SelectionMenuScene::create_deck_info(const rect_f32& rect) {
 }
 void SelectionMenuScene::create_deck_infos() {
     float umbral = 0.14f;
-    rect_f32 r = {{ 0.0285f, 0.3f  }, { 0.3f, 0.1425f }};
+    rect_f32 r = {{ 0.0375f, 0.385f }, { 0.325f, 0.14f }};
     for (int i = 0; i < _num_cards_of_deck; ++i) {
         create_deck_info(r); 
         r.position.y += umbral;
@@ -363,7 +363,7 @@ void SelectionMenuScene::create_deck_infos() {
 }
 void SelectionMenuScene::create_weapon_info() {
    // rect_f32 rect = {{1.3f, 0.25f} ,{0.75f, 0.5f}};
-    rect_f32 rect = { {0.695f, 0.225f} ,{0.3f, 0.25f} };
+    rect_f32 rect = { {0.695f, 0.315f} ,{0.3f, 0.25f} };
     ecs::entity_t e = create_entity(
         ecs::grp::UI,
         ecs::scene::SELECTIONMENUSCENE,
