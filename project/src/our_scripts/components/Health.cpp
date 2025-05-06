81550a9a8a9a2a4f90488042e4689bac28be6141
Óscar Melquiades Durán Narganes <osduran@ucm.es>
2025-05-06 23:58:15 +0200

    Shield se renderiza

--- project/src/our_scripts/components/Health.cpp
+++ project/src/our_scripts/components/Health.cpp
@@ -95,6 +95,10 @@ void Health::resetCurrentHeatlh()
 }
 int
 Health::getHealth() const { return _currentHealth; }
+int Health::getShield() const
+{
+	return _shield;
+}
 void 
 Health::takeShield(int shield) {
 	_shield = shield;
