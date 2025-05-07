c757bd3a3a9a12bf3bf24e622209d0eec898d52b
Izan <izandeve@ucm.es>
2025-05-07 17:56:11 +0200

    balanced wm

--- project/src/our_scripts/components/WaveManager.h
+++ project/src/our_scripts/components/WaveManager.h
@@ -32,11 +32,11 @@ struct enemy_spawn_definition {
 };
 //index equals enemyType
 constexpr enemy_spawn_definition enemy_spawn_data[8] = {
-    {2,2}, //0
+    {1,1}, //0
     {1,1}, //1
     {1,1}, //2
     {1,2}, //3
-    {3,1}, //4
+    {3,2}, //4
     {1,4}, //5
     {1,3}, //6
     {1,3}  //7
